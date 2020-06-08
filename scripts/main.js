101012b7530e7534e3293ebdae9530833670152e
cagloria <33134245+cagloria@users.noreply.github.com>
2020-06-08 13:00:14 -0700

    Updated header on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 function toggleNav() {
     $(".nav-button").toggleClass("nav-button--open-nav");
-    $(".nav-cover").toggleClass("nav-cover--open-nav");
-    $("nav").toggleClass("open-nav");
+    // $(".nav-cover").toggleClass("nav-cover--open-nav");
+    // $("nav").toggleClass("open-nav");
 }
 
-window.onresize = function() {
+window.onresize = function () {
     if (window.innerWidth > 768) {
         $(".nav-button").removeClass("nav-button--open-nav");
-        $(".nav-cover").removeClass("nav-cover--open-nav");
-        $("nav").removeClass("open-nav");
+        // $(".nav-cover").removeClass("nav-cover--open-nav");
+        // $("nav").removeClass("open-nav");
     }
 };
