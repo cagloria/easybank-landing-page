a62501f1066400a857d1e06a2f12423b8035d389
cagloria <33134245+cagloria@users.noreply.github.com>
2020-06-19 18:06:39 -0700

    Added tablet navigation

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,18 @@ function toggleNav() {
     $("nav").toggleClass("open-nav");
 }
 
+function removeNav() {
+    $(".nav-button").removeClass("nav-button--open-nav");
+    $(".nav-cover").removeClass("nav-cover--open-nav");
+    $("nav").removeClass("open-nav");
+}
+
 window.onresize = function () {
     if (window.innerWidth > 768) {
-        $(".nav-button").removeClass("nav-button--open-nav");
-        $(".nav-cover").removeClass("nav-cover--open-nav");
-        $("nav").removeClass("open-nav");
+        removeNav();
     }
+};
+
+window.onscroll = function () {
+    removeNav();
 };
