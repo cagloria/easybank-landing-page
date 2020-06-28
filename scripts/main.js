2d01908530f9d7b6acef64907a779d8dfeef38df
cagloria <33134245+cagloria@users.noreply.github.com>
2020-06-28 15:03:16 -0700

    Allowed use of the navigation menu via keyboard
    - Created a function to check if the mobile nav is open or if the screen is in desktop size
    - Navigation will set the tabindex of its links to 0 if so
    - Sets it to -1 if mobile menu is closed
    - Adjusted alt descriptions of two icons

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,62 @@
+/**
+ * Toggles the classes for navigation items to define if the nav menu is open or
+ * closed.
+ */
 function toggleNav() {
     $(".nav-button").toggleClass("nav-button--open-nav");
     $(".nav-cover").toggleClass("nav-cover--open-nav");
     $("nav").toggleClass("open-nav");
+    setNavTabIndex();
 }
 
+/**
+ * Removes the classes for navigation items to remove indication that the nav
+ * menu is open.
+ */
 function removeNav() {
     $(".nav-button").removeClass("nav-button--open-nav");
     $(".nav-cover").removeClass("nav-cover--open-nav");
     $("nav").removeClass("open-nav");
 }
 
+/**
+ * Checks if the nav element has the class "open-nav" or if the window is at a
+ * width of greater than 768px. If either condition applies, the nav links are
+ * placed in the tab index. If not, the links are removed from the tab order.
+ */
+function setNavTabIndex() {
+    const $navList = $(".nav-list").find("a");
+    if ($("nav").hasClass("open-nav") || window.innerWidth > 768) {
+        $navList.attr("tabindex", "0");
+    } else {
+        removeNav();
+        $navList.attr("tabindex", "-1");
+    }
+}
+
+/**
+ * Checks if the window moves into desktop layout, and so switches from mobile
+ * navigation to desktop.
+ */
 window.onresize = function () {
     if (window.innerWidth > 768) {
         removeNav();
     }
+    setNavTabIndex();
 };
 
+/**
+ * Closes the mobile navigation on scroll.
+ */
 window.onscroll = function () {
     removeNav();
+    setNavTabIndex();
+};
+
+/**
+ * Calls setNavLinksTabIndex() to check if the window is loaded in a desktop or
+ * tablet/mobile layout.
+ */
+window.onload = function () {
+    setNavTabIndex();
 };
